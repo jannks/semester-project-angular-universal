ac40a697021cac34a1f2e7935659e1c5c0ccffb4
jannks <jankessler@aikq.de>
2018-03-11 00:04:39 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyBrH5MQO58Heaz1REIS2upkatdg7AgAgNw',
-    authDomain: 'semester-project-angular.firebaseapp.com',
-    databaseURL: 'https://semester-project-angular.firebaseio.com',
-    projectId: 'semester-project-angular',
-    storageBucket: 'semester-project-angular.appspot.com',
-    messagingSenderId: '434689440789'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
